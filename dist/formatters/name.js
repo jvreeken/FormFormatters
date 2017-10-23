9b195ee5b3fcc6c1c8d2d0fc727cbc9b9d71d518
Mike Hoffert <mike.hoffert@gmail.com>
2017-10-23 11:09:05 -0600

    Dist files

---
+++ dist/formatters/name.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var _simpleTranslator = require("simple-translator");
+
+var _simpleTranslator2 = _interopRequireDefault(_simpleTranslator);
+
+var _string = require("./string");
+
+var _string2 = _interopRequireDefault(_string);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var NameFormatter = {
+  format: function format(value) {
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+    var _StrFormatter$format = _string2.default.format(value, options),
+        valid = _StrFormatter$format.valid,
+        parsed = _StrFormatter$format.parsed,
+        formatted = _StrFormatter$format.formatted,
+        errors = _StrFormatter$format.errors;
+
+    parsed = parsed.toLowerCase();
+    formatted = formatted.toLowerCase();
+    if (valid && parsed.length > 0) {
+      var arr = parsed.split(" ").map(function (item) {
+        return item.charAt(0).toUpperCase() + item.slice(1);
+      });
+      parsed = arr.join(" ");
+      formatted = parsed;
+    }
+
+    return {
+      valid: valid,
+      parsed: parsed,
+      formatted: formatted,
+      errors: errors
+    };
+  }
+};
+
+module.exports = NameFormatter;
