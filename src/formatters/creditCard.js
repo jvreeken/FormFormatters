88588dd154e420d9cf33e738743322465e4fcd8b
James Vreeken <james.vreeken@tazworks.com>
2018-05-24 11:31:44 -0600

    Added Credit Card Formatter and Mask

---
+++ src/formatters/creditCard.js
@@ -0,0 +1,28 @@
+import StrFormatter from "./string";
+
+const CreditCardFormatter = {
+  format(value, options = {}) {
+    let{valid, parsed, formatted, errors} = StrFormatter.format(value, options);
+
+    if(valid && parsed.length > 0) {
+      // remove all non-digits
+      parsed = parsed.replace(/\D/g, "");
+      formatted = parsed.replace(/^(\d{4})(\d{4})(\d{4})(\d{4})$/, "$1-$2-$3-$4");
+      if(parsed.length !== 16) {
+        valid = false;
+        parsed = value;
+        formatted = value;
+        errors.push("FormFormatters.creditCardInvalid");
+      }
+    }
+
+    return({
+      valid,
+      parsed,
+      formatted,
+      errors
+    });
+  }
+};
+
+module.exports = CreditCardFormatter;
