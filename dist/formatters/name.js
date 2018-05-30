9b14d06f9d156e809aeaa4d9711f35b21d31cc11
Mike Hoffert <mike.hoffert@gmail.com>
2018-05-30 08:51:34 -0600

    Dist files

--- dist/formatters/name.js
+++ dist/formatters/name.js
@@ -10,11 +10,11 @@ var NameFormatter = {
   format: function format(value) {
     var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-    var _StringFormatter$form = _string2.default.format(value, options),
-        valid = _StringFormatter$form.valid,
-        parsed = _StringFormatter$form.parsed,
-        formatted = _StringFormatter$form.formatted,
-        errors = _StringFormatter$form.errors;
+    var _StrFormatter$format = _string2.default.format(value, options),
+        valid = _StrFormatter$format.valid,
+        parsed = _StrFormatter$format.parsed,
+        formatted = _StrFormatter$format.formatted,
+        errors = _StrFormatter$format.errors;
 
     parsed = parsed.toLowerCase();
     formatted = formatted.toLowerCase();
